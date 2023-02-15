ce5ca5faaaea3656ad90295e42bddee9c08b0939
Viktoriant <osal17@yandex.ru>
2023-02-15 17:59:46 +0300

    На Юбитер сделан новый файл в соответствии с ДЗ

--- Beautifulsoup.py
+++ Beautifulsoup.py
@@ -1,39 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://www.cbr.ru/scripts/XML_daily.asp"
-response = requests.get(url)
-
-soup = BeautifulSoup(response.content, 'lxml')
-print(soup)
-
-lst = soup.find_all('name')
-
-for item in lst:
-    print(item)
-
-url = "https://matplotlib.org/stablibe/gallery/index.html"
+url = "https://seaborn.pydata.org/examples/index.html"
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 print(soup)
 
 
-lst = soup.find_all('h2')
+lst = soup.find_all("p")
 
 for item in lst:
     print(item)
 
 def clean_item(my_item):
     position = my_item.find('<a')
-    return my_item[4:position]
+    return my_item[3:position]
 
-print()
+print("")
 
 for item in lst:
     print(clean_item(str(item)))
           
-lst = soup.find_all(class_='std std-ref')
+lst = soup.find_all(class_='thumb-label')
 
 
 for item in lst:
@@ -41,9 +30,9 @@ for item in lst:
 
 def clean_item2(my_item):
     position = my_item.find('</')
-    return my_item[26:position]
+    return my_item[30:position]
 
-print()
+print("")
 
 for item in lst:
     print(clean_item2(str(item)))
